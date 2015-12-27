607fde131f058cf7ed5fa81d5044ace88eb07c89
Duncan Hamilton <Duncan@duncans-macbook-pro.local>
2015-12-27 00:11:55 -0500

    Initial Commit

---
+++ Super cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super cool
+//
+//  Created by Duncan Hamilton on 2015-12-27.
+//  Copyright © 2015 Duncan Hamilton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
